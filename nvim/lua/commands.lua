618a2d3547c744c701f7797439b6d13007f29f55
Akshay <108661354+akshidas@users.noreply.github.com>
2024-08-30 23:40:17 +0530

    added a minimal custom status line

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,19 +1,3 @@
--- local function progress()
--- 	local cur = vim.fn.line(".")
--- 	local total = vim.fn.line("$")
--- 	if cur == 1 then
--- 		return "Top"
--- 	elseif cur == total then
--- 		return "Bot"
--- 	else
--- 		return string.format("%2d%%", math.floor(cur / total * 100))
--- 	end
--- end
---
--- vim.api.nvim_create_user_command("Prg", function()
--- 	print(progress())
--- end, {})
---
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
@@ -22,4 +6,151 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- vim.cmd.colorscheme("retrobox")
+local modes = {
+	["n"] = "NORMAL",
+	["no"] = "NORMAL",
+	["v"] = "VISUAL",
+	["V"] = "VISUAL LINE",
+	[""] = "VISUAL BLOCK",
+	["s"] = "SELECT",
+	["S"] = "SELECT LINE",
+	[""] = "SELECT BLOCK",
+	["i"] = "INSERT",
+	["ic"] = "INSERT",
+	["R"] = "REPLACE",
+	["Rv"] = "VISUAL REPLACE",
+	["c"] = "COMMAND",
+	["cv"] = "VIM EX",
+	["ce"] = "EX",
+	["r"] = "PROMPT",
+	["rm"] = "MOAR",
+	["r?"] = "CONFIRM",
+	["!"] = "SHELL",
+	["t"] = "TERMINAL",
+}
+
+local function mode()
+	local current_mode = vim.api.nvim_get_mode().mode
+	return string.format(" %s ", modes[current_mode]):upper()
+end
+
+local function update_mode_colors()
+	local current_mode = vim.api.nvim_get_mode().mode
+	local mode_color = "%#StatusLineAccent#"
+	if current_mode == "n" then
+		mode_color = "%#StatuslineAccent#"
+	elseif current_mode == "i" or current_mode == "ic" then
+		mode_color = "%#StatuslineInsertAccent#"
+	elseif current_mode == "v" or current_mode == "V" or current_mode == "" then
+		mode_color = "%#StatuslineVisualAccent#"
+	elseif current_mode == "R" then
+		mode_color = "%#StatuslineReplaceAccent#"
+	elseif current_mode == "c" then
+		mode_color = "%#StatuslineCmdLineAccent#"
+	elseif current_mode == "t" then
+		mode_color = "%#StatuslineTerminalAccent#"
+	end
+	return mode_color
+end
+
+local function filepath()
+	local fpath = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.:h")
+	if fpath == "" or fpath == "." then
+		return " "
+	end
+
+	return string.format(" %%<%s/", fpath)
+end
+
+local function filename()
+	local fname = vim.fn.expand("%:t")
+	if fname == "" then
+		return ""
+	end
+	return fname .. " "
+end
+
+local function lsp()
+	local count = {}
+	local levels = {
+		errors = "Error",
+		warnings = "Warn",
+		info = "Info",
+		hints = "Hint",
+	}
+
+	for k, level in pairs(levels) do
+		count[k] = vim.tbl_count(vim.diagnostic.get(0, { severity = level }))
+	end
+
+	local errors = ""
+	local warnings = ""
+	local hints = ""
+	local info = ""
+
+	if count["errors"] ~= 0 then
+		errors = " %#LspDiagnosticsSignError# " .. count["errors"]
+	end
+	if count["warnings"] ~= 0 then
+		warnings = " %#LspDiagnosticsSignWarning# " .. count["warnings"]
+	end
+	if count["hints"] ~= 0 then
+		hints = " %#LspDiagnosticsSignHint#󰞋 " .. count["hints"]
+	end
+	if count["info"] ~= 0 then
+		info = " %#LspDiagnosticsSignInformation# " .. count["info"]
+	end
+
+	return errors .. warnings .. hints .. info .. "%#Normal#"
+end
+
+
+local function lineinfo()
+	if vim.bo.filetype == "alpha" then
+		return ""
+	end
+	return " %l:%c "
+end
+
+local function timeInfo()
+	return os.date("%d %b %a %H:%M", os.time())
+end
+
+Statusline = {}
+
+Statusline.active = function()
+	return table.concat({
+		"%#Statusline#",
+		update_mode_colors(),
+		mode(),
+		"%#Normal# ",
+		filepath(),
+		filename(),
+		"%#Normal#",
+		lsp(),
+		"%#Normal#%=",
+		timeInfo(),
+		" %#StatusLineExtra#",
+		lineinfo(),
+	})
+end
+
+function Statusline.inactive()
+	return " %F"
+end
+
+function Statusline.short()
+	return "%#StatusLineNC#   NvimTree"
+end
+
+vim.api.nvim_exec(
+	[[
+	  augroup Statusline
+	  au!
+	  au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
+  au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
+  au WinEnter,BufEnter,FileType NvimTree setlocal statusline=%!v:lua.Statusline.short()
+  augroup END
+	]],
+	false
+)
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,11 +6,6 @@ config.window_decorations = "RESIZE"
 config.font = wezterm.font("Monaspace Argon", { weight = 600 })
 config.font_size = 18
 config.enable_tab_bar = false
-config.colors = {
-	foreground = "grey",
-	background = "#14161b",
-	cursor_bg = "grey",
-	cursor_fg = "#14161b",
-	cursor_border = "#52ad70",
-}
+config.color_scheme = 'Catppuccin Mocha (Gogh)'
+
 return config
