a1f4c243ec8d0721d10552e6457a9afe1c65b153
Akshay <108661354+akshidas@users.noreply.github.com>
2024-09-17 18:16:45 +0530

    added a base 16 theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,8 +1,19 @@
 return {
-	"rebelot/kanagawa.nvim",
-	priority = 1000,
+	{
+		"rebelot/kanagawa.nvim",
+		enabled = false,
+		priority = 1000,
+		--
+		-- config = function()
+		-- 	vim.cmd.colorscheme("kanagawa")
+		-- end,
+	},
 
-	config = function()
-		vim.cmd.colorscheme("kanagawa")
-	end,
+	{
+		"metalelf0/base16-black-metal-scheme",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("base16-black-metal-bathory")
+		end,
+	},
 }
