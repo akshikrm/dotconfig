9b6cc02ad0334533cf30b04c104064ea28a7a020
Akshay <108661354+akshidas@users.noreply.github.com>
2024-09-30 17:53:15 +0530

    some changes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,9 @@
 return {
 	{
-		"rebelot/kanagawa.nvim",
-		enabled = false,
-		priority = 1000,
-		--
-		-- config = function()
-		-- 	vim.cmd.colorscheme("kanagawa")
-		-- end,
-	},
-
-	{
 		"metalelf0/base16-black-metal-scheme",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("base16-black-metal-bathory")
+			vim.cmd.colorscheme("base16-black-metal")
 		end,
 	},
 }
