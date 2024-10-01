c06b20f74f1c500792ac35b988d8d04dc89718fa
Akshay <108661354+akshidas@users.noreply.github.com>
2024-10-02 00:13:06 +0530

    fixed the tsserver issue

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,6 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = {"BufReadPre", "BufNewFile"},
+	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 	},
@@ -22,19 +22,14 @@ return {
 				-- opts.desc = "Go to declaration"
 				-- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-
-
-
 				opts.desc = "[C]ode [A]ction"
 				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
 				opts.desc = "[R]ename"
 				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-
 				opts.desc = "[E]xpand [D]iagnostics"
 				keymap.set("n", "<leader>ed", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
 
 				opts.desc = "Show documentation for what is under cursor"
 				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
@@ -42,7 +37,6 @@ return {
 				opts.desc = "Restart LSP"
 				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 			end,
-
 		})
 
 		local capabilities = cmp_nvim_lsp.default_capabilities()
@@ -50,11 +44,16 @@ return {
 		mason_lspconfig.setup_handlers({
 			-- default handler for installed servers
 			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
+				if server_name == "tsserver" then
+					lspconfig["ts_ls"].setup({
+						capabilities = capabilities,
+					})
+				else
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end
 			end,
 		})
-
 	end,
 }
