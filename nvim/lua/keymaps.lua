ba173e8b8539d7b45767d9b28c110bb11a27cec0
Akshay <108661354+akshidas@users.noreply.github.com>
2024-10-25 18:09:24 +0530

    made some changes to keybindings

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "[F]ile [E]xplorer" })
-vim.keymap.set("n", "<leader>gf", vim.cmd.GFiles)
-vim.keymap.set("n", "<leader>pf", vim.cmd.Files)
-vim.keymap.set("n", "<leader>a", vim.cmd.Buffers)
+vim.keymap.set("n", "<C-f>", vim.cmd.GFiles, { desc = "Open files tracked by git" })
+vim.keymap.set("n", "<C-p>", vim.cmd.Files({ desc = "Open files in current working directory" }))
+vim.keymap.set("n", "<C-x>", vim.cmd.Buffers, { desc = "Show a list of currently open buffers" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump half a page and down align the line to middle" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump half a page and up align the line to middle" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Just to the next search term and move the line to the middle" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Just to the previous search term and move the line to the middle" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
