cc1478cc3d81bbf0274ef9a77f72c7211f7931f6
Akshay <108661354+akshidas@users.noreply.github.com>
2024-10-25 15:57:38 +0530

    removed deleted imports

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 require("options")
 require("commands")
 require("keymaps")
-require("statusline")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
