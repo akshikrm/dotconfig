c34fda1b607c690f2b34694bca46c0a736168f72
Akshay Krishna <akshikrm@gmail.com>
2024-12-12 11:57:28 +0530

    Add lauline

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,4 +1,4 @@
-local group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true })
+local group = vim.api.nvim_create_augroup("akshikrm-highlight-yank", { clear = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
@@ -9,21 +9,4 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- uim.api.nvim_create_autocmd("BufWritePost", {
--- 	desc = "Format File on saving",
--- 	group = group,
--- 	callback = function()
--- 		vim.lsp.buf.format()
--- 	end,
--- })
-
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "markdown",
--- 	callback = function()
--- 		print("opened markdown file")
--- 		vim.lsp.start({
--- 			name = "my-markdown-server",
--- 			cmd = { "/Users/raven/golang-lsp/dist" }
--- 		})
--- 	end
--- })
+-- vim.cmd("colorscheme onedark")
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,10 @@
 return {
 	{
+		enabled = true,
 		"metalelf0/base16-black-metal-scheme",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("base16-black-metal")
+			vim.cmd("colorscheme base16-black-metal")
 		end,
 	},
 }
