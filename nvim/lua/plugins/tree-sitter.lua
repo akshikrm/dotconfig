730e77def7eed6f71410fd99bd09d066f7a5fbdd
Akshay Krishna <akshikrm@gmail.com>
2025-08-16 11:06:54 +0530

    add some new languages to treesitter

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -6,7 +6,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "vimdoc", "javascript", "typescript", "solidity", "go", "html" },
+			ensure_installed = { "vimdoc", "javascript", "typescript", "c", "go", "html", "css", "gitcommit", "gitignore", "git_config", "json" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
